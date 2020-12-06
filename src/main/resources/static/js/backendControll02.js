a321ece2df34caf68f860ab57e3f3536ed6e52bf
Bill_Chou <bill.chou@tpisoftware.com>
2020-12-07 00:01:25 +0800

    增加 修改地點 頁面

---
+++ src/main/resources/static/js/backendControll02.js
@@ -0,0 +1,153 @@
+let map;
+
+$(document).ready(function(){
+	init();
+});
+
+function init() {
+	initView();
+	initMap();
+	initButton();
+}
+
+function initView() {
+	$("#menu-toggle").click(function(e) {
+     	e.preventDefault();
+     	$("#wrapper").toggleClass("toggled");
+ 	});
+
+ 	$(window).resize(function(e) {
+   		if($(window).width()<=768){
+     		$("#wrapper").removeClass("toggled");
+   		}else{
+     		$("#wrapper").addClass("toggled");
+   		}
+ 	});
+}
+
+function initMap() {
+	var mapOption = {
+		center: { lat: 0, lng: 0 },
+		zoom: 10,
+	}
+	map = new google.maps.Map(document.getElementById("map"), mapOption);
+	
+	//取得使用者位置
+	gettingPosition()
+		.then(position => successCallback(map, position))
+		.catch(error => errorCallback(error));
+		
+	queryMarkers("A", "A");
+}
+
+function queryMarkers(parentId, sonId) {	
+	//query markers
+	var data = {};
+	data.DATA={};
+	
+	if(parentId === "AllCountry") {
+		parentId = sonId;
+		sonId = "A";
+	}
+	data.DATA.COUNTRY_ID=parentId;
+	data.DATA.CITY_ID=sonId;
+	var jsonData = JSON.stringify(data);
+	var result = sendRequest("POST", "application/json", "/marker/queryByKind", jsonData, "json", null);
+	resultMarkers = result.DATA.MARKERS;
+	
+	clearMarkersArray();
+	resultMarkers.forEach(marker => {
+		let latlon = {
+			lng: marker.LONGITUDE, 
+	        lat: marker.LATITUDE
+		};
+		let resultMark = setMarker(map, latlon, marker.SN, 1);
+		let infoMsg = "<div align='left'><b>名稱："+ marker.NAME +"</div>";
+		setMarkerInfo(map, resultMark, infoMsg);
+		
+		pushMarkersArray(resultMark);
+	});
+	
+	let content;
+	const bounds = new google.maps.LatLngBounds();
+	let latlon;
+	
+	$('.list-group').empty();
+	resultMarkers.forEach(marker => {
+		let latlon = {
+			lng: parseFloat(marker.LONGITUDE), 
+	        lat: parseFloat(marker.LATITUDE)
+		};
+		bounds.extend(latlon);
+		content = "名稱："+ marker.NAME +"<br>"+ marker.COMMENT;
+		$('.list-group').append("<a class='list-group-item list-group-item-action markerList' id='"+ marker.SN +"' target='_blank'>"+ content +"</a>");
+	});
+	map.fitBounds(bounds);
+
+	initButton();
+}
+
+function initButton() {
+	$('.markerList').bind("click", function() {
+		let result = resultMarkers.filter(marker => marker.SN == this.id);
+		let latlon = {
+			lng: parseFloat(result[0].LONGITUDE), 
+	        lat: parseFloat(result[0].LATITUDE)
+		};
+
+		map.panTo(latlon);
+		map.setZoom(15);
+	});
+	
+	$('.markerKindList a').unbind("click").bind("click", function() {
+		queryMarkers($(this).closest('.markerKindList').attr('id'), this.id);
+	});
+}
+
+function queryMarkers(parentId, sonId) {	
+	//query markers
+	var data = {};
+	data.DATA={};
+	
+	if(parentId === "AllCountry") {
+		parentId = sonId;
+		sonId = "A";
+	}
+	data.DATA.COUNTRY_ID=parentId;
+	data.DATA.CITY_ID=sonId;
+	var jsonData = JSON.stringify(data);
+	var result = sendRequest("POST", "application/json", "/marker/queryByKind", jsonData, "json", null);
+	resultMarkers = result.DATA.MARKERS;
+	
+	clearMarkersArray();
+	resultMarkers.forEach(marker => {
+		let latlon = {
+			lng: marker.LONGITUDE, 
+	        lat: marker.LATITUDE
+		};
+		let resultMark = setMarker(map, latlon, marker.SN, 1);
+		let infoMsg = "<div align='left'><b>名稱："+ marker.NAME +"</div>";
+		setMarkerInfo(map, resultMark, infoMsg);
+		
+		pushMarkersArray(resultMark);
+	});
+	
+	let content;
+	const bounds = new google.maps.LatLngBounds();
+	let latlon;
+	
+	$('.list-group').empty();
+	resultMarkers.forEach(marker => {
+		let latlon = {
+			lng: parseFloat(marker.LONGITUDE), 
+	        lat: parseFloat(marker.LATITUDE)
+		};
+		bounds.extend(latlon);
+		content = "名稱："+ marker.NAME +"<br>"+ marker.COMMENT;
+		$('.list-group').append("<a class='list-group-item list-group-item-action markerList' id='"+ marker.SN +"' target='_blank'>"+ content +"</a>");
+	});
+	map.fitBounds(bounds);
+
+	initButton();
+}
+
